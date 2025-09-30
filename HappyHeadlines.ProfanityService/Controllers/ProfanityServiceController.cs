8b601ae0e15d1fabe1d2f9eccb3f600e94c7380f
Ronnie Rogert Stentoft Damsgaard <rondam@oes.dk>
2025-09-30 19:41:30 +0200

    Logging for profanity word result

--- HappyHeadlines.ProfanityService/Controllers/ProfanityServiceController.cs
+++ HappyHeadlines.ProfanityService/Controllers/ProfanityServiceController.cs
@@ -18,7 +18,17 @@ namespace HappyHeadlines.ProfanityService.Controllers
         [HttpPost("check")]
         public async Task<IActionResult> CheckProfanity([FromBody] string text)
         {
-            bool hasProfanity = await _profanityService.ContainsProfanityAsync(text);
+            bool hasProfanity = false;
+            try
+            {
+                hasProfanity = await _profanityService.ContainsProfanityAsync(text);
+            }
+            catch (Exception ex)
+            {
+                MonitorService.MonitorService.Log.Error("Error checking profanity: {Error}", ex.Message);
+                return BadRequest($"Error checking profanity: {ex.Message}");
+            }
+            MonitorService.MonitorService.Log.Information("Checked text for profanity. Result: {HasProfanity}", hasProfanity);
             return Ok(new { hasProfanity });
         }
 
@@ -26,7 +36,7 @@ namespace HappyHeadlines.ProfanityService.Controllers
         public async Task<IActionResult> AddProfanityWord([FromBody] string word)
         {
             var entity = new ProfanityWord { Word = word };
-            _profanityService.AddWord(entity); // You need to implement AddWord in your service
+            _profanityService.AddWord(entity); // You need to implement AddWord in your service, but is out of scope for now
             return Ok();
         }
     }
